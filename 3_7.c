e67155af332f04a7fe2bcb8e64ab884760f58e26
jk <vargsmane@gmx.de>
2016-12-20 10:11:48 +0100

    + started 3_7, 4_1 + started b_tree

---
+++ 3_7.c
@@ -0,0 +1,72 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct heap_node HeapNode;
+
+struct heap_node {
+    int value;
+    HeapNode *left;
+    HeapNode *right;
+};
+
+void print_array(int *array, size_t length) {
+    for (size_t i = 0; i < length; ++i) {
+        printf("%d ", array[i]);
+    }
+    printf("\n");
+}
+
+void swap(int *array, size_t a, size_t b) {
+    int temp = array[a];
+    array[a] = array[b];
+    array[b] = temp;
+}
+
+
+void max_heapify(int *array, size_t i, size_t heap_size) {
+    size_t left = 2 * i;
+    size_t right = 2 * i + 1;
+    size_t max;
+    if ((left <= heap_size) && (array[left] > array[right])) {
+        max = left;
+    } else {
+        max = i;
+    }
+    if ((right <= heap_size) && (array[right] > array[max])) {
+        max = right;
+    }
+    if (max != i) {
+        swap(array, i, max);
+        max_heapify(array, max, heap_size);
+    }
+}
+
+void build_max_heap(int *array, size_t heap_size) {
+    for (size_t i = (size_t) (heap_size / 2); i < heap_size / 2; --i) {
+        max_heapify(array, i, heap_size);
+    }
+}
+
+int main() {
+    int array[] = {1,2,3,4,5,6,7,8};
+    build_max_heap(array, 8);
+    print_array(array, 8);
+    return 0;
+}
+
+/*
+ *
+ * max-heapify (A,i,heap-groesse) {
+l = lkind(i);
+r = rkind(i);
+if ((l <= heap-groesse) && (A[l] > A[i]))
+max = l;
+else max = i;
+if { (r <= heap-groesse) && (A[r] > A[max])
+max = r;
+if ( max != i) {
+vertausche A[i] und A[max];
+max-heapify(A,max,heap-groesse);
+}
+}
+ * */
--- data_structures.h
+++ data_structures.h
@@ -853,4 +853,282 @@ void print_alist(AdjacencyList *list) {
     printf("\n");
 }
 
+
+/* ### B-TREE ### */
+
+
+// DATA STRUCTURES
+
+typedef struct b_tree_node BTreeNode;
+
+typedef struct b_tree_node_array {
+    size_t length;
+    BTreeNode **elements;
+} BTreeNodeArray;
+
+struct b_tree_node {
+    size_t degree;
+    size_t last_key_index;
+    BTreeNodeArray *children;
+    IntegerArray *keys;
+};
+
+typedef struct b_tree {
+    BTreeNode *root;
+    size_t degree;
+} BTree;
+
+// CONSTRUCTORS AND DESTRUCTORS
+
+/// Destroy a given node array
+/// \param nodeArray The node array to destroy
+/// \return NULL
+BTreeNodeArray *b_tree_node_array_destroy(BTreeNodeArray *nodeArray) {
+    if (nodeArray) {
+        free(nodeArray->elements);
+    }
+    free(nodeArray);
+    return NULL;
+}
+
+/// Creates an array of nodes with a given length
+/// \param length The length of the array
+/// \return A pointer to the new array or NULL if memory allocation failed
+BTreeNodeArray *b_tree_node_array_create(size_t length) {
+    BTreeNodeArray *result = malloc(sizeof(BTreeNodeArray));
+    if (result == NULL) {
+        return NULL;
+    }
+
+    result->elements = calloc(length, sizeof(BTreeNode));
+    if (result->elements == NULL) {
+        return b_tree_node_array_destroy(result);
+    }
+
+    result->length = length;
+
+    return result;
+}
+
+/// Destroys a node and its child nodes recursively
+/// \param node The node to destroy
+/// \return A NULL pointer to clean up the node pointer
+BTreeNode *b_tree_node_array_destroy_recursively(BTreeNode *node) {
+    if (node) {
+        node->keys = destroy_integer_array(node->keys);
+        node->children = b_tree_node_array_destroy(node->children);
+    }
+    free(node);
+    return NULL;
+}
+
+/// Creates a new node of given degree
+/// \param degree The degree of the node
+/// \return A pointer to the new node or NULL in case of memory allocation failure
+BTreeNode *b_tree_node_create(size_t degree) {
+
+    BTreeNode *result = malloc(sizeof(BTreeNode));
+    if (result == NULL) {
+        return NULL;
+    }
+
+    result->keys = create_integer_array(degree - 1);
+    result->children = b_tree_node_array_create(degree);
+    if (result->keys == NULL || result->children == NULL) {
+        return b_tree_node_array_destroy_recursively(result);
+    }
+
+    result->degree = degree;
+    result->last_key_index = 0;
+
+    return result;
+}
+
+/// Creates a new n-ary tree
+/// \param degree The degree
+/// \return A pointer to the new tree or NULL in case of memory error or if the degree is invalid (lower than 2)
+BTree *b_tree_create(size_t degree) {
+    if (degree < 2) {
+        return NULL;
+    }
+
+    BTree *result = malloc(sizeof(BTree));
+    if (result == NULL) {
+        return NULL;
+    }
+
+    result->root = NULL;
+    result->degree = degree;
+    return result;
+}
+
+/// Destroys the tree, recursively destroying its nodes
+/// \param tree The tree to destroy
+/// \return A NULL pointer to clean up the tree pointer
+BTree *b_tree_destroy(BTree *tree) {
+    if (tree) {
+        tree->root = b_tree_node_array_destroy_recursively(tree->root);
+    }
+    free(tree);
+    return NULL;
+}
+
+// FUNCTIONS
+
+/// Prints tree_node keys, also showing how many of the key slots are still unset
+/// \param node The node whose keys are to be printed
+void b_tree_node_keys_print(BTreeNode *node) {
+    printf("[");
+    for (size_t j = 0; j < node->keys->length; ++j) {
+        if (j < node->last_key_index) {
+            printf("%d", node->keys->elements[j]);
+        } else {
+            printf(" ");
+        }
+
+        if (j != node->keys->length - 1) {
+            printf(", ");
+        }
+    }
+    printf("]\n");
+}
+
+/// Checks in which child tree_node the value may fit, or if it fits inside the key array.
+/// Will create a new child tree_node if necessary.
+/// \param node The node from which to start insertion
+/// \param value The value to insert
+/// \return 0 if insertion was successful or 1 if creating a new node was impossible due to memory error and 2 if the
+/// given node was NULL and 3 if the function terminates unexpectedly
+int b_tree_add_internal(BTreeNode *node, int value) {
+
+    if (!node) {
+        return 2;
+    }
+
+    for (size_t i = 0; i < node->degree - 1; i++) {
+
+        if (i == node->last_key_index) {
+            node->keys->elements[i] = value;
+            node->last_key_index += 1;
+            return 0;
+        }
+        else if (node->keys->elements[i] >= value) {
+            if (node->children->elements[i] != NULL) {
+                return b_tree_add_internal(node->children->elements[i], value);
+            }
+            else {
+                node->children->elements[i] = b_tree_node_create(node->degree);
+                if (node->children->elements[i] == NULL) {
+                    return 1;
+                }
+                node->children->elements[i]->keys->elements[0] = value;
+                node->children->elements[i]->last_key_index += 1;
+                return 0;
+            }
+        }
+        else if (i == node->degree - 2){
+            if (node->children->elements[i + 1] != NULL) {
+                return b_tree_add_internal(node->children->elements[i + 1], value);
+            } else {
+                node->children->elements[i + 1] = b_tree_node_create(node->degree);
+                if (node->children->elements[i + 1] == NULL) {
+                    return 1;
+                }
+                node->children->elements[i + 1]->keys->elements[0] = value;
+                node->children->elements[i + 1]->last_key_index += 1;
+                return 0;
+            }
+        }
+    }
+
+    return 3;
+}
+
+/// Adds a new value to the tree and creates a root tree_node if the tree is empty
+/// \param tree The tree to extend
+/// \param value The value to insert
+/// \return 0 if insertion was successful, else 1 in case of memory error
+int b_tree_add(BTree *tree, int value) {
+    if (tree->root == NULL) {
+        tree->root = b_tree_node_create(tree->degree);
+        if (tree->root == NULL) {
+            return 1;
+        }
+        tree->root->keys->elements[0] = value;
+        tree->root->last_key_index = 1;
+    } else {
+        if (b_tree_add_internal(tree->root, value)) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+/// Prints all the nodes recursively, indenting them for better readability
+/// \param node The node to start printing from
+/// \param depth The depth of printing, this should be 1 initially
+void b_tree_nodes_print(BTreeNode *node, size_t depth){
+    if (node) {
+        for (size_t key_index = 0; key_index < node->last_key_index; key_index++) {
+            if (node->children->elements[key_index] != NULL) {
+                b_tree_nodes_print(node->children->elements[key_index], depth + 1);
+            }
+        }
+
+        for (size_t indentations = 1; indentations < depth; indentations++) {
+            printf("\t");
+        }
+
+        b_tree_node_keys_print(node);
+
+        if (node->children->elements[node->degree - 1]) {
+            b_tree_nodes_print(node->children->elements[node->degree - 1], depth + 1);
+        }
+    }
+}
+
+/// Prints all the nodes of a tree recursively, indenting them for better readability
+/// \param tree The tree to print
+void b_tree_print(BTree *tree) {
+    b_tree_nodes_print(tree->root, 1);
+}
+
+///
+/// \param node
+/// \param value
+/// \return
+BTreeNode *b_tree_search_internal(BTreeNode *node, int value) {
+
+    if (!node) {
+        return NULL;
+    }
+
+    for (size_t i = 0; i < node->last_key_index; ++i) {
+
+        // Check if value is in the keys
+        if (node->keys->elements[i] == value) {
+            return node;
+        }
+
+            // Else check if it's too small to fit, so we need to go down the left subtree
+        else if (value < node->keys->elements[i]) {
+            return b_tree_search_internal(node->children->elements[i], value);
+        }
+
+            // Else check if we are at the last key
+        else if (i == node->degree - 2) {
+            // Now value is bigger than the last element and could only be in the right child
+            // Only the last key in the array can have a right child
+            return b_tree_search_internal(node->children->elements[i + 1], value);
+        }
+    }
+    return NULL;
+}
+
+// Returns a pointer to the containing TreeNode or NULL, if value not in graph
+BTreeNode *b_tree_search(BTree *tree, int value) {
+    return b_tree_search_internal(tree->root, value);
+}
+
+
 #endif
