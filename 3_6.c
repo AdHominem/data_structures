87db4fdf87303013f3340ea0346b5e8445313471
AdHominem <vargsmane@gmx.de>
2016-12-23 14:34:34 +0100

    Finalized

--- 3_6.c
+++ 3_6.c
@@ -1,15 +1,29 @@
 #include "data_structures.h"
-#include <string.h>
 #include <time.h>
 
-size_t get_max_chain_length(int input[TABLE_SIZE], size_t (*hash_function)(const int)) {
-    HashTable *hash_table = hash_table_create(hash_function);
+size_t hash_modulo(const int *parameters) {
+    return (size_t) (parameters[0] % TABLE_SIZE);
+}
+
+// prime 491 is used because its > 128, which is the size of the hash table
+size_t hash_universal(const int *parameters) {
+    return ((parameters[1] * (size_t) parameters[0] + parameters[2]) % 491) % TABLE_SIZE;
+}
+
+size_t get_max_chain_length(int input[TABLE_SIZE], size_t (*hash_function)(const int *), int *hash_parameters) {
+    HashTable *hash_table = (hash_function == hash_modulo)
+                            ? hash_table_create(hash_modulo, 0) : hash_table_create(hash_universal, 2);
     if (!hash_table) {
         return 0;
     }
 
     for (size_t i = 0; i < TABLE_SIZE; ++i) {
-        hash_table_add(hash_table, input[i]);
+        int parameters[1 + hash_table->argument_count];
+        parameters[0] = input[i];
+        for (size_t j = 1; j < hash_table->argument_count; ++j) {
+            parameters[j] = hash_parameters[j - 1];
+        }
+        hash_table_add(hash_table, parameters);
     }
 
     size_t maximum_chain_length = 0;
@@ -24,23 +38,10 @@ size_t get_max_chain_length(int input[TABLE_SIZE], size_t (*hash_function)(const
     return maximum_chain_length;
 }
 
-size_t hash_modulo(const int value) {
-    return (size_t) (value % TABLE_SIZE);
-}
-
-// prime 491 is used because its > 128, which is the size of the hash table
-size_t hash_universal(const int value) {
-    const size_t a = (size_t) (rand() % 491 + 1);
-    const size_t b = (size_t) (rand() % 491);
-    //printf("a: %zu, b:%zu\n", a, b);
-    return ((a * (size_t) value + b) % 491) % TABLE_SIZE;
-}
-
 int main() {
-
     srand((unsigned) time(NULL));
-    size_t global_maximum_chain_length = 0;
-    size_t total_maximum_chain_length = 0;
+    size_t maximum_chain_length = 0;
+    size_t sum_of_maximum_chain_lengths = 0;
     int *maximum_chain_input;
     const size_t attempts = 100;
 
@@ -52,42 +53,45 @@ int main() {
             input[i] = rand();
         }
 
-        size_t max_chain_length = get_max_chain_length(input, hash_modulo);
-        if (!max_chain_length) {
+        size_t current_maximum_chain_length = get_max_chain_length(input, hash_modulo, NULL);
+        if (!current_maximum_chain_length) {
             perror("An error occurred during memory allocation!");
             return 1;
         }
 
-        if (max_chain_length > global_maximum_chain_length) {
-            global_maximum_chain_length = max_chain_length;
+        if (current_maximum_chain_length > maximum_chain_length) {
+            maximum_chain_length = current_maximum_chain_length;
             maximum_chain_input = input;
         }
-        total_maximum_chain_length += max_chain_length;
+        sum_of_maximum_chain_lengths += current_maximum_chain_length;
     }
 
-    printf("Average maximum chain length: %lf\n", total_maximum_chain_length / (double) attempts);
-    printf("Longest chain was: %zu\n", global_maximum_chain_length);
+    printf("Average maximum chain length: %lf\n", sum_of_maximum_chain_lengths / (double) attempts);
+    printf("Longest chain was: %zu\n", maximum_chain_length);
 
     // Now use the input creating the longest chain and use it 100 times with universal hashing instead
-    total_maximum_chain_length = 0;
-    global_maximum_chain_length = 0;
+    sum_of_maximum_chain_lengths = 0;
+    maximum_chain_length = 0;
+
+    int params[2];
+    while (!(params[0] = rand() % 491));
+    params[1] = rand() % 491;
+
     for (size_t k = 0; k < attempts; ++k) {
-        size_t max_chain_length = get_max_chain_length(maximum_chain_input, hash_universal);
-        if (!max_chain_length) {
+        size_t current_maximum_chain_length = get_max_chain_length(maximum_chain_input, hash_universal, params);
+        if (!current_maximum_chain_length) {
             perror("An error occurred during memory allocation!");
             return 1;
         }
 
-        if (max_chain_length > global_maximum_chain_length) {
-            global_maximum_chain_length = max_chain_length;
-        }
-        total_maximum_chain_length += max_chain_length;
+        maximum_chain_length = (current_maximum_chain_length > maximum_chain_length)
+                                      ? current_maximum_chain_length : maximum_chain_length;
+        sum_of_maximum_chain_lengths += current_maximum_chain_length;
     }
 
     printf("Average maximum chain length (universal hashing): %lf\n",
-           total_maximum_chain_length / (double) attempts);
-    printf("Longest chain was: %zu\n", global_maximum_chain_length);
-
+           sum_of_maximum_chain_lengths / (double) attempts);
+    printf("Longest chain was: %zu\n", maximum_chain_length);
 
     return 0;
 }
