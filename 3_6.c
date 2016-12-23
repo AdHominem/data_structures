a3c7d7f41c9ac423bab7ac6ad0ff5ac77429900a
AdHominem <vargsmane@gmx.de>
2016-12-23 11:32:23 +0100

    Use a pointer instead of memcpy to reference the maximum_chain_input

--- 3_6.c
+++ 3_6.c
@@ -2,7 +2,7 @@
 #include <string.h>
 #include <time.h>
 
-size_t get_max_chain_length(int input[TABLE_SIZE], size_t (*hash_function)(int)) {
+size_t get_max_chain_length(int input[TABLE_SIZE], size_t (*hash_function)(const int)) {
     HashTable *hash_table = hash_table_create(hash_function);
     if (!hash_table) {
         return 0;
@@ -14,7 +14,7 @@ size_t get_max_chain_length(int input[TABLE_SIZE], size_t (*hash_function)(int))
 
     size_t maximum_chain_length = 0;
     for (int j = 0; j < TABLE_SIZE; ++j) {
-        size_t length = length_of_(hash_table->rows[j]);
+        const size_t length = length_of_(hash_table->rows[j]);
         maximum_chain_length = (length > maximum_chain_length) ? length : maximum_chain_length;
     }
 
@@ -24,15 +24,16 @@ size_t get_max_chain_length(int input[TABLE_SIZE], size_t (*hash_function)(int))
     return maximum_chain_length;
 }
 
-size_t modulo_table_size(int value) {
+size_t hash_modulo(const int value) {
     return (size_t) (value % TABLE_SIZE);
 }
 
-size_t universal_hashing(int value) {
-    size_t a = (size_t) (rand() % 491 + 1);
-    size_t b = (size_t) (rand() % 491);
+// prime 491 is used because its > 128, which is the size of the hash table
+size_t hash_universal(const int value) {
+    const size_t a = (size_t) (rand() % 491 + 1);
+    const size_t b = (size_t) (rand() % 491);
     //printf("a: %zu, b:%zu\n", a, b);
-    return ((a * value + b) % 491) % TABLE_SIZE;
+    return ((a * (size_t) value + b) % 491) % TABLE_SIZE;
 }
 
 int main() {
@@ -40,8 +41,8 @@ int main() {
     srand((unsigned) time(NULL));
     size_t global_maximum_chain_length = 0;
     size_t total_maximum_chain_length = 0;
-    int maximum_chain_input[TABLE_SIZE];
-    size_t attempts = 100;
+    int *maximum_chain_input;
+    const size_t attempts = 100;
 
     // Try 100 random input arrays of size 128 each and calculate the average longest chain and the total longest chain
     for (size_t k = 0; k < attempts; ++k) {
@@ -51,14 +52,15 @@ int main() {
             input[i] = rand();
         }
 
-        size_t max_chain_length = get_max_chain_length(input, modulo_table_size);
+        size_t max_chain_length = get_max_chain_length(input, hash_modulo);
         if (!max_chain_length) {
+            perror("An error occurred during memory allocation!");
             return 1;
         }
 
         if (max_chain_length > global_maximum_chain_length) {
             global_maximum_chain_length = max_chain_length;
-            memcpy(maximum_chain_input, input, TABLE_SIZE);
+            maximum_chain_input = input;
         }
         total_maximum_chain_length += max_chain_length;
     }
@@ -70,8 +72,9 @@ int main() {
     total_maximum_chain_length = 0;
     global_maximum_chain_length = 0;
     for (size_t k = 0; k < attempts; ++k) {
-        size_t max_chain_length = get_max_chain_length(maximum_chain_input, universal_hashing);
+        size_t max_chain_length = get_max_chain_length(maximum_chain_input, hash_universal);
         if (!max_chain_length) {
+            perror("An error occurred during memory allocation!");
             return 1;
         }
 
@@ -84,6 +87,7 @@ int main() {
     printf("Average maximum chain length (universal hashing): %lf\n",
            total_maximum_chain_length / (double) attempts);
     printf("Longest chain was: %zu\n", global_maximum_chain_length);
+
 
     return 0;
 }
