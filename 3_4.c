9e21d74876a9771a43f7c93cf0237ad7678c671f
AdHominem <vargsmane@gmx.de>
2017-01-05 10:31:39 +0100

    Finalized

--- 3_4.c
+++ 3_4.c
@@ -1,48 +1,58 @@
-#include "algorithms.h"
 #include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 #define SIZE 1000
 #define SIZE_TO_SEARCH 100
 
-int binary_search_modified(const int *array, const size_t size, const int number, size_t *iterations) {
+void measure_performance(const char *name, const int *array, const int *to_search,
+                         int (*function)(const int *, const size_t, const int, size_t *)) ;
+int binary_search_modified(const int *array, const size_t size, const int number, size_t *iterations) ;
+int interpolation_search_modified(const int *array, const size_t size, const int key, size_t *iterations) ;
+size_t test_searching_algorithm(const int array[SIZE], const int to_search[SIZE_TO_SEARCH],
+                                int (*algorithm)(const int*, const size_t, const int, size_t*)) ;
+void quicksort(int *array, size_t size) ;
+void swap(int *array, size_t a, size_t b) ;
 
-    size_t last = size;
-    size_t first = 0;
+int main() {
 
-    while (first <= last) {
-        ++*iterations;
-        size_t middle = first + (last - first) / 2;
-
-        if (number == array[middle]) {
-            return 1;
-        } else if (number > array[middle]) {
-            first = middle + 1;
-        } else {
-            last = middle - 1;
-        }
+    // Generate an array with random values between 0 and 9999
+    srand((unsigned) time(NULL));
+    int array[SIZE];
+    for (size_t i = 0; i < SIZE; ++i) {
+        array[i] = rand() % 10000;
     }
+
+    // Determine 100 random values to search for out of the array
+    // (chose the first 100, they are random because the array is unsorted)
+    int to_search[SIZE_TO_SEARCH];
+    for (size_t k = 0; k < SIZE_TO_SEARCH; ++k) {
+        to_search[k] = array[k];
+    }
+
+    quicksort(array, SIZE);
+
+    measure_performance("Binary Search", array, to_search, binary_search_modified);
+    measure_performance("Interpolation", array, to_search, interpolation_search_modified);
+
+    // Determine 100 real random values to search for
+    for (size_t k = 0; k < SIZE_TO_SEARCH; ++k) {
+        to_search[k] = rand() % 10000;
+    }
+
+    measure_performance("Binary Search Random", array, to_search, binary_search_modified);
+    measure_performance("Interpolation Random", array, to_search, interpolation_search_modified);
 
     return 0;
 }
 
-int interpolation_search_modified(const int *array, const size_t size, const int key, size_t *iterations) {
-    size_t low = 0;
-    size_t high = size - 1;
-    size_t middle;
+void measure_performance(const char *name, const int *array, const int *to_search,
+                         int (*function)(const int *, const size_t, const int, size_t *)) {
+    size_t iterations = test_searching_algorithm(array, to_search, function);
 
-    while (array[high] != array[low] && key >= array[low] && key <= array[high]) {
-        ++*iterations;
-        middle = low + ((key - array[low]) * (high - low) / (array[high] - array[low]));
-        if (array[middle] < key) {
-            low = middle + 1;
-        } else if (key < array[middle]) {
-            high = middle - 1;
-        } else {
-            return 1;
-        }
-    }
-
-    return key == array[low];
+    printf("\n%s:\n", name);
+    printf("Iterations: %zu\n", iterations);
+    printf("Average: %f\n", iterations / (double) SIZE_TO_SEARCH);
 }
 
 /// Uses the specified search algorithm so look for a set of values in a larger array
@@ -58,34 +68,79 @@ size_t test_searching_algorithm(const int array[SIZE], const int to_search[SIZE_
     return iterations;
 }
 
-int main() {
+int binary_search_modified(const int *array, const size_t size, const int number, size_t *iterations) {
 
-    // Generate an array with random values between 0 and 9999
-    srand((unsigned) time(NULL));
-    int array[SIZE];
-    for (size_t i = 0; i < SIZE; ++i) {
-        array[i] = rand() % 10000;
+    int upper_bound = (int) size - 1;
+    int lower_bound = 0;
+
+    while (lower_bound <= upper_bound) {
+        ++*iterations;
+
+        int middle = lower_bound + (upper_bound - lower_bound) / 2;
+
+        if (number == array[middle]) {
+            return 1;
+        } else if (number > array[middle]) {
+            lower_bound = middle + 1;
+        } else /*if (number < array[middle]) */ {
+            upper_bound = middle - 1;
+        }
     }
-
-    // Determine 100 random values to search for (random because the array is yet to be sorted)
-    int to_search[SIZE_TO_SEARCH];
-    for (size_t k = 0; k < SIZE_TO_SEARCH; ++k) {
-        to_search[k] = array[k];
-    }
-
-    quicksort(array, SIZE);
-
-    size_t iterations = test_searching_algorithm(array, to_search, binary_search_modified);
-
-    printf("\nBinary Search:\n");
-    printf("Iterations: %zu\n", iterations);
-    printf("Average: %f\n", iterations / (double) SIZE_TO_SEARCH);
-
-    iterations = test_searching_algorithm(array, to_search, interpolation_search_modified);
-
-    printf("\nInterpolation:\n");
-    printf("Iterations: %zu\n", iterations);
-    printf("Average: %f\n", iterations / (double) SIZE_TO_SEARCH);
 
     return 0;
 }
+
+int interpolation_search_modified(const int *array, const size_t size, const int key, size_t *iterations) {
+
+    if (size == 0) {
+        return 0;
+    }
+
+    int lower_bound = 0;
+    int upper_bound = (int) size - 1;
+    int middle;
+
+    while (array[upper_bound] != array[lower_bound]
+           && key >= array[lower_bound]
+           && key <= array[upper_bound]) {
+        ++*iterations;
+        middle = lower_bound + ((key - array[lower_bound]) * (upper_bound - lower_bound)
+                                / (array[upper_bound] - array[lower_bound]));
+        if (array[middle] < key) {
+            lower_bound = middle + 1;
+        } else if (key < array[middle]) {
+            upper_bound = middle - 1;
+        } else {
+            return 1;
+        }
+    }
+
+    return key == array[lower_bound];
+}
+
+void quicksort(int *array, size_t size) {
+    if (size < 2) return;
+
+    // This part determines the pivot element and places all array elements to the LEFT of the pivot index
+    // which are smaller and all the ones which are bigger to the RIGHT
+    int pivot = array[size / 2];
+    size_t i, j;
+    for (i = 0, j = size - 1; ; i++, j--) {
+        while (array[i] < pivot) i++;
+        while (array[j] > pivot) j--;
+        if (i >= j) break;
+        swap(array, i, j);
+    }
+
+    quicksort(array, i);
+    quicksort(array + i, size - i);
+}
+
+void swap(int *array, size_t a, size_t b) {
+    int temp = array[a];
+    array[a] = array[b];
+    array[b] = temp;
+}
+
+
+
