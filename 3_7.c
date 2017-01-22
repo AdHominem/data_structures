d017269ba203b8737286eacc2316ab6f8510a398
jk <vargsmane@gmx.de>
2017-01-22 16:53:03 +0100

    Finalized for Windows

--- 3_7.c
+++ 3_7.c
@@ -6,10 +6,10 @@
 
 void priority_queue_insert(int *queue, const int value, size_t *size) ;
 int priority_queue_remove_max(int *queue, size_t *size) ;
-void priority_queue_increase(int *queue, size_t index, const int increment) ;
-int verify(const int *heap, const size_t size);
-void max_heapify(int *array, size_t index, size_t heap_size);
-void swap(int *array, size_t a, size_t b);
+void priority_queue_increase(int *queue, const size_t index, const int increment) ;
+int heap_verify(const int *heap, const size_t size);
+void heap_max_heapify(int *array, const size_t index, const size_t heap_size);
+void swap(int *array, const size_t a, const size_t b);
 
 int main() {
     const size_t queue_limit = 200;
@@ -22,7 +22,7 @@ int main() {
         priority_queue_insert(priority_queue, rand() % 100 + 1, &queue_size);
     }
 
-    assert(verify(priority_queue, queue_size));
+    assert(heap_verify(priority_queue, queue_size));
 
     // perform 100 random operations on the queue
     for (size_t j = 0; j < 100; ++j) {
@@ -35,7 +35,7 @@ int main() {
         } else {
             priority_queue_insert(priority_queue, rand() % 100 + 1, &queue_size);
         }
-        assert(verify(priority_queue, queue_size));
+        assert(heap_verify(priority_queue, queue_size));
     }
 
     return 0;
@@ -53,17 +53,22 @@ int priority_queue_maximum(const int *queue) {
     return queue[0];
 }
 
-void priority_queue_increase(int *queue, size_t index, const int increment) {
+/// Increases the queue element at index by an increment, restoring queue property afterwards
+/// \param queue The priority queue to operate on
+/// \param index The index of the element to increment
+/// \param increment The increment size
+void priority_queue_increase(int *queue, const size_t index, const int increment) {
     size_t parent_index = (size_t) ceil(index / 2.0) - 1;
     queue[index] += increment;
-    while (index > 0 && queue[parent_index] < queue[index]) {
-        swap(queue, index, parent_index);
-        index = parent_index;
-        parent_index = (size_t) ceil(index / 2.0) - 1;
+    size_t child_index = index;
+    while (child_index > 0 && queue[parent_index] < queue[child_index]) {
+        swap(queue, child_index, parent_index);
+        child_index = parent_index;
+        parent_index = (size_t) ceil(child_index / 2.0) - 1;
     }
 }
 
-/// Swap the first with the last element and restored the heap on the reduced queue
+/// Swap the first with the last element and restores the heap property on the reduced queue
 /// This virtually truncates the queue by one, since size is decremented
 /// \param queue The queue whose largest element is to remove
 /// \param size A pointer to the queue's size
@@ -72,12 +77,12 @@ int priority_queue_remove_max(int *queue, size_t *size) {
     int max = queue[0];
     queue[0] = queue[*size - 1];
     --*size;
-    max_heapify(queue, 0, *size);
+    heap_max_heapify(queue, 0, *size);
     return max;
 }
 
-int verify(const int *heap, const size_t size) {
-    // loop over all leafs
+/// Verifies that the heap property is fulfilled for a given heap
+int heap_verify(const int *heap, const size_t size) {
     for (size_t i = size / 2; i < size; ++i) {
         size_t index = i;
         size_t parent_index = (size_t) ceil(index / 2.0) - 1;
@@ -97,7 +102,7 @@ int verify(const int *heap, const size_t size) {
 /// \param array The heap to check as an array
 /// \param index The index to start checking at
 /// \param heap_size The size of the heap
-void max_heapify(int *array, size_t index, size_t heap_size) {
+void heap_max_heapify(int *array, const size_t index, const size_t heap_size) {
     size_t left_child_index = 2 * index + 1;
     size_t right_child_index = 2 * index + 2;
 
@@ -109,7 +114,7 @@ void max_heapify(int *array, size_t index, size_t heap_size) {
     }
     if (maximum != index) {
         swap(array, index, maximum);
-        max_heapify(array, maximum, heap_size);
+        heap_max_heapify(array, maximum, heap_size);
     }
 
 }
@@ -117,17 +122,17 @@ void max_heapify(int *array, size_t index, size_t heap_size) {
 /// Constructs a binary heap out of an arbitrary array.
 /// \param array The array to build the heap of
 /// \param size The size of the array
-void build_max_heap(int *array, size_t size) {
+void heap_build_max(int *array, const size_t size) {
     if (size < 2) return;
 
     // Note that it is only necessary to heapify the top (size / 2 - 1) elements,
     // since the lower half will only contain leafs
     for (size_t i = size / 2; i-- != 0;) {
-        max_heapify(array, i, size);
+        heap_max_heapify(array, i, size);
     }
 }
 
-void swap(int *array, size_t a, size_t b) {
+void swap(int *array, const size_t a, const size_t b) {
     int temp = array[a];
     array[a] = array[b];
     array[b] = temp;
